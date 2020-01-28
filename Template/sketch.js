0df02869e06aa2bf550436846484dc55e8ff222f
jameswcastro <48529786+jameswcastro@users.noreply.github.com>
2020-01-28 11:34:18 -0500

    PaintingSketch

---
+++ Template/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  background(1000,1000,1000);
+  rectMode(CENTER);
+  strokeWeight(5);
+  //xpos ypos width height
+  fill("red");
+  rect(140,140,200,200);
+  fill("black");
+  rect(90,290,100,100);
+  fill("white");
+  rect(190,265,100,50);
+  rect(190,315,100,50);
+  fill("black");
+  rect(190,352.5,100,25);
+  fill("blue");
+  rect(300,327.5,120,75);
+  fill("white")
+  rect(300,265,120,50);
+  rect(270,185,60,110);
+  rect(330,185,60,110);
+}
