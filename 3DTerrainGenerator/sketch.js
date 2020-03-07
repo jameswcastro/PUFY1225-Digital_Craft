579be1af5b32752fd437cf542b926d30a6d2d790
jameswcastro <48529786+jameswcastro@users.noreply.github.com>
2020-03-07 13:46:27 -0500

    Add files via upload

--- 3DTerrainGenerator/sketch.js
+++ 3DTerrainGenerator/sketch.js
@@ -7,24 +7,31 @@ function setup() {
   cols = width/scl;
   rows = height/scl;
   //create 2D array called terrain to map perlin noise z values
-  
+  yoff = 0;
   for (y=0; y<rows; y++){
-    terrain[y] = [random(-10,10)];
+    terrain[y] = [map(noise(yoff), 0,1, -50,50)];
+    xoff = 0;
     for(x=0; x<cols;x++){
-      terrain[y][x] = [random(-7,7)];
+      terrain[y][x] = [map(noise(xoff,yoff), 0,1, -50,50)];
+      xoff += 0.1;
   print(terrain);
     }
+  yoff += 0.1;
   }
 }
 
 function draw() {
+  //light follows mouse around scene
+    //let lx = mouseX - width / 2;
+    //let ly = mouseY - height / 2;
+    //pointLight(250, 250, 250, lx, ly, 50);
   //pretty colors & presentation (change for laser cut)
   frameRate(1);
   background(200);
-  stroke(255);
+  stroke(225);
   fill("green");
   //rotating & translating mesh
-  translate(-width/2,-height/3,30);
+  translate(-width/2,-height/3,0);
   rotateX(45);
   //drawing mesh with nested for loop of rows and columns. Assigning z values using 2D array
     for (y=0; y<rows-1; y++){
