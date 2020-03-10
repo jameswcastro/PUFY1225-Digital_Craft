0ce39c09fc6b498987d10e9e26a0e7ec5bd584b0
jameswcastro <48529786+jameswcastro@users.noreply.github.com>
2020-03-10 11:55:03 -0400

    Add files via upload

---
+++ ImportMushroom/sketch.js
@@ -0,0 +1,20 @@
+let mushroom;
+function preload() {
+    mushroom = loadModel('Assets/DeathCapMushroom.obj');
+}
+
+function setup() {
+    createCanvas(400,400,WEBGL);
+    
+}
+
+function draw() {
+    background(220);
+    scale(100);
+    translate(0,1.5,0)
+    //rotateX(frameCount * 0.01);
+    rotateZ(PI);
+    rotateY(frameCount * 0.01);
+    normalMaterial();
+    model(mushroom);
+}
